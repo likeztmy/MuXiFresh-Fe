427b1f996bd672ad43f9d6a0c406db647df263d5
Old-Second <63891262+Old-Second@users.noreply.github.com>
2022-08-04 01:43:41 +0800

    base

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -8,7 +8,7 @@ const Header = () => {
    <div className='header'>
       <div className='muxi'>
         <img src={muxi} alt="muxi" className='logo' ></img>
-        <div className='text'>木犀</div>
+        <div className='text'>木 犀</div>
       </div>
       <div className='avatar'>
           
--- src/Components/Join/Join.tsx
+++ src/Components/Join/Join.tsx
@@ -3,41 +3,46 @@ import J_progress from '../J_progress';
 import './Join.less';
 
 const Join = () => {
-  
-const [flag,setFlag]=useState('a');
-    
 
-function Select(e:React.MouseEvent<HTMLDivElement> ):any{
+    const [flag, setFlag] = useState('a');
+
+
+    function Select(e: React.MouseEvent<HTMLDivElement>): any {
         e.preventDefault();
-        const div:HTMLDivElement=e.currentTarget;
-        console.log(div.id)
+        const div: HTMLDivElement = e.currentTarget;
         setClickedDiv(div.id);
-}
+        // alert(window.innerWidth)
+    }
 
-function setClickedDiv(id:string):void{
-    if(id=='a')
-        setFlag('a')
-    if(id=='b')
-        setFlag('b')
-    if(id=='c')
-        setFlag('c')
-    console.log(flag=='b')
-}
+    function setClickedDiv(id: string): void {
+        if (id == 'a')
+            setFlag('a')
+        if (id == 'b')
+            setFlag('b')
+        if (id == 'c')
+            setFlag('c')
+        if (id == 'd')
+            setFlag('d')
+        if (id == 'e')
+            setFlag('e')
+    }
 
-  return (
-   <div className='body'>
-    <div className='box'> 
-    <div className="top">
-        <div className={flag=='a'?'selected':"title"} id='a' onClick={(event)=>Select(event)} >报名表查看</div>
-        <div className={flag=='b'?'selected':"title"} id='b' onClick={(event)=>Select(event)} >进度查询</div>
-        <div className={flag=='c'?'selected':"title"} id='c' onClick={(event)=>Select(event)} >作业</div>
-    </div>
-    {/* 组件  */} 
-      <J_progress/>
-  
-    </div>
-   </div>
-  )
+    return (
+        <div className='body'>
+            <div className='box'>
+                <div className="top">
+                    <div className={flag == 'a' ? 'selected' : "title"} id='a' onClick={(event) => Select(event)} >报名表查看</div>
+                    <div className={flag == 'b' ? 'selected' : "title"} id='b' onClick={(event) => Select(event)} >进度查询</div>
+                    <div className={flag == 'c' ? 'selected' : "title"} id='c' onClick={(event) => Select(event)} >作业</div>
+                    <div className={flag == 'd' ? 'selected' : "title"} id='d' onClick={(event) => Select(event)} >审阅</div>
+                    <div className={flag == 'e' ? 'selected' : "title"} id='e' onClick={(event) => Select(event)} >作业发布</div>
+                </div>
+                {/* 组件  */}
+                <J_progress />
+
+            </div>
+        </div>
+    )
 };
 
 export default Join;
