3f74cce44af67e16bf8998d48edb1a413bc5394b
Old-Second <oldsecond2@qq.com>
2022-08-28 01:07:34 +0800

    finish

--- src/Components/AJ_form/form.tsx
+++ src/Components/AJ_form/form.tsx
@@ -3,6 +3,6 @@ import Mine_form from '../Mine_form'
 
 export default function J_form() {
   return (
-      <Mine_form/>
+    <Mine_form />
   )
 }
--- src/Components/Mine_form/index.tsx
+++ src/Components/Mine_form/index.tsx
@@ -1,28 +1,28 @@
 //无论是visitor还是manager，查看本人报名表
-import React, {useState, useEffect} from 'react'
+import React ,{useState,useEffect}from 'react'
 import default_avatar from '../../images/default_avatar.png'
-import {getJson} from '../../interface/fetch'
+import { getJson } from '../../interface/fetch'
 import './index.less'
 
-const Mine_form: React.FC = () => {
-    const [name, setName] = useState('')
-    const [student_id, setStudent_id] = useState('')
-    const [college, setCollege] = useState('')
-    const [major, setMajor] = useState('')
-    const [grade, setGrade] = useState('')
-    const [gender, setGender] = useState('')
-    const [email, setEmail] = useState('')
-    const [contact_way, setContact_way] = useState('')
-    const [contact_number, setContact_number] = useState('')
-    const [group, setGroup] = useState('')
-    const [reason, setReason] = useState('')
-    const [understand, setUnderstand] = useState('')
-    const [self_introduction, setSelf_introduction] = useState('')
-    const [if_other_organization, setIf_other_organization] = useState('')
-    const [avatar, setAvatar] = useState(default_avatar)
+const Mine_form:React.FC = ()=>{
+    const [name,setName]=useState('')
+    const [student_id,setStudent_id]=useState('')
+    const [college,setCollege]=useState('')
+    const [major,setMajor]=useState('')
+    const [grade,setGrade]=useState('')
+    const [gender,setGender]=useState('')
+    const [email,setEmail]=useState('')
+    const [contact_way,setContact_way]=useState('')
+    const [contact_number,setContact_number]=useState('')
+    const [group,setGroup]=useState('')
+    const [reason,setReason]=useState('')
+    const [understand,setUnderstand]=useState('')
+    const [self_introduction,setSelf_introduction]=useState('')
+    const [if_other_organization,setIf_other_organization]=useState('')
+    const [avatar,setAvatar]=useState(default_avatar)
 
-    useEffect(() => {
-        getJson('/form/view').then(res => {
+    useEffect(()=>{
+        getJson('/form/view').then(res=>{
             console.log(res.data)
             setName(res.data.name)
             setStudent_id(res.data.student_id)
@@ -40,7 +40,7 @@ const Mine_form: React.FC = () => {
             setIf_other_organization(res.data.if_other_organization)
             setAvatar(res.data.avatar)
         })
-    }, [])
+    },[])
     return (
         <div className="container">
             <div className="theme">我的简历</div>
@@ -50,8 +50,7 @@ const Mine_form: React.FC = () => {
                     <div className="highlight"></div>
                 </div>
                 <div className="body">
-                    <div className="picture"><img src={avatar == '' ? default_avatar : avatar} alt=""
-                                                  className='avatar'/></div>
+                    <div className="picture"><img src={avatar==''? default_avatar : avatar} alt="" className='avatar'/></div>
                     <div className="detail">
                         <div className="line">姓名：<span className='circle_one'>{name}</span></div>
                         <div className="line"><span>学号：</span><span className='circle_one'>{student_id}</span></div>
@@ -59,10 +58,10 @@ const Mine_form: React.FC = () => {
                         <div className="line">专业：<span className='circle_one'>{major}</span></div>
                         <div className="line">年级：<span className='circle_one'>{grade}</span></div>
                         <div className="line">性别：<span className='circle_two'>{gender}</span></div>
-                        <div>联系方式：<span className='circle_two'>邮箱</span>
+                        <div >联系方式：<span className='circle_two'>邮箱</span>
                             <div className="circle_one line_2">{email}</div>
                             {/* QQ 手机号选填 */}
-                            <div className="circle_two line">{contact_way}</div>
+                            <div className="circle_two line">{contact_way}</div> 
                             <div className="circle_one">{contact_number}</div>
                         </div>
                     </div>
@@ -74,23 +73,19 @@ const Mine_form: React.FC = () => {
                     <div className="highlight"></div>
                 </div>
                 <div className="body">
-                    <div className="row"><span> &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;心动组别：</span><span
-                        className='circle_two'>{group}</span></div>
-                    <div className="row"><span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;心动理由：</span><span
-                        className='circle_three'>{reason}</span></div>
-                    <div className="row_1"><span>对组别的了解：</span><span className='circle_three'>{understand}</span>
-                    </div>
+                    <div className="row"><span> &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;心动组别：</span><span className='circle_two'>{group}</span></div>
+                    <div className="row"><span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;心动理由：</span><span className='circle_three'>{reason}</span></div>
+                    <div className="row_1"><span>对组别的了解：</span><span className='circle_three'>{understand}</span></div>
                 </div>
             </div>
             <div className="three">
-                <div className="head">
+            <div className="head">
                     <div className="title">自述部分</div>
                     <div className="highlight"></div>
-                </div>
-                <div className="body">
-                    <div className="intro"><span>自我介绍：</span><span
-                        className='circle_three'>{self_introduction}</span></div>
-                </div>
+            </div>
+            <div className="body">
+                <div className="intro"><span>自我介绍：</span><span className='circle_three'>{self_introduction}</span></div>
+            </div>
             </div>
             <div className="four">
                 <div className="head">
@@ -101,13 +96,13 @@ const Mine_form: React.FC = () => {
                     <div className="qus">你是否有加入/正在加入一些其他组织或担任学生工作？</div>
                     <div className="ans">
                         <span>
-                            <span className={if_other_organization == 'True' ? 'yes' : 'no'}>
+                            <span className={if_other_organization=='True'? 'yes' : 'no'}>
                                 <span className='inCircle'></span>
                             </span>
                             &nbsp;&nbsp;是
                         </span>
                         <span>
-                            <span className={if_other_organization != 'True' ? 'yes' : 'no'}>
+                            <span className={if_other_organization!='True'?'yes':'no'}>
                                 <span className='inCircle'></span>
                             </span>
                             &nbsp;&nbsp;否
